89576eee6c435a9e748c0918dff698a4b07bb492
Lenovo <Lenovo@DESKTOP-KHV87JG>
2023-08-23 08:18:55 +0530

    1stcommit

---
+++ Maven_Demo/src/test/java/Pratice/Demo1Test.java
@@ -0,0 +1,12 @@
+package Pratice;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+	@Test(groups="RegressionTest")
+	public void Demo1Test()
+	{
+		System.out.println("Maven Test");
+	}
+
+}
